8500b7fb1c36f9f2aa05d02182229a9dddba4383
Karol Perechowski <karol.perechowski@gmail.com>
2018-07-13 12:24:30 +0200

    front page changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
 
+const appRoutes:Routes = [
+  {path: "details", component: DetailsComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
